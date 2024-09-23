20ccfb829756b396186367458a8b631f56e2c317
Ilmir Nigmatullin <nigmatullin.mir@gmail.com>
2024-09-23 22:20:26 +0500

    delete uuid column and change name columns in table account

--- bot/database/models/account.py
+++ bot/database/models/account.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import BigInteger, ForeignKey, String, Uuid, text
+from sqlalchemy import BigInteger, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from bot.database import Base
@@ -16,11 +16,8 @@ class Account(Base):
     avito_user_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
     avito_client_id: Mapped[str] = mapped_column(String, nullable=False)
     avito_client_secret: Mapped[str] = mapped_column(String, nullable=False)
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    uuid: Mapped[Uuid] = mapped_column(
-        Uuid, nullable=False, server_default=text("gen_random_uuid()")
-    )
-    status: Mapped[bool] = mapped_column(nullable=False, default=False)
+    account_name: Mapped[str] = mapped_column(String, nullable=False)
+    account_status: Mapped[bool] = mapped_column(nullable=False, default=False)
     account_slot: Mapped["AccountSlot"] = relationship(
         back_populates="account", uselist=False, lazy="selectin"
     )
